aedbe145690b47642cfb7300b664996c05492e9f
cibelipetrone <cibeli.petrone@gmail.com>
2025-05-20 10:22:59 -0300

    refactor: add json dumps

--- src/lambda/lambda_hello/hello_terraform.py
+++ src/lambda/lambda_hello/hello_terraform.py
@@ -1,8 +1,10 @@
+import json
+
 def lambda_handler(event, context):
     return success_response("Hello, Terraform!")
 
 def success_response(message):
-    return{
-        "statusCode":200,
-        "body":message
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"message": message})  
     }
