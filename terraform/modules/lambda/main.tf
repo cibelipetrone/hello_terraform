9a395ee4920f6bd1041832f8964f4ed71b627a7a
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2025-05-21 06:36:19 -0300

    feat: update AWS Lambda function environment variables to include Cognito User Pool ID and Client ID

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,12 +1,17 @@
+# Configuração do AWS Cognito User Pool
 resource "aws_lambda_function" "this" {
   function_name = var.function_name
   handler       = var.handler
   runtime       = var.runtime
   role          = var.role_arn
-  filename          = var.filename
-  source_code_hash  = filebase64sha256(var.filename)
+  filename      = var.filename
+  source_code_hash = filebase64sha256(var.filename)
 
   environment {
-    variables = var.environment_variables
+    variables = merge(var.environment_variables, {
+      COGNITO_USER_POOL_ID = var.cognito_user_pool_id
+      COGNITO_CLIENT_ID    = var.cognito_client_id
+      TABLE_NAME           = "shopping_list"
+    })
   }
 }
